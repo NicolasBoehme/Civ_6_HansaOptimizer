8453f38bb20e4c5f3de34f73399587e1bea4841c
R3V4N <mausemaelken@gmx.de>
2026-04-24 19:12:37 +0200

    add isNeighbour function stub

--- Board.py
+++ Board.py
@@ -1,3 +1,4 @@
+from typing import Tuple, Optional
 from Tile import Tile
 from River import River
 import numpy as np
@@ -5,5 +6,11 @@ class Board:
     rivers = list[River]
     tiles = np.array([],dtype=Tile)
 
-    def tile_coords(tile:Tile) -> tuple[int,int]:
+    def tile_coords(tile:Tile) -> Tuple[int,int]:
+        pass
+
+    def getNeighbours(tile:Tile) -> Tuple[Optional[Tile], Optional[Tile], Optional[Tile], Optional[Tile], Optional[Tile], Optional[Tile]]:
+        pass
+
+    def isNeighbour(tile:Tile) -> bool:
         pass
