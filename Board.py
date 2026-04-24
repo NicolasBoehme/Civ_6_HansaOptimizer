9086651b0bf52839cc2dcad1737083aa7426c4e6
R3V4N <mausemaelken@gmx.de>
2026-04-24 19:04:37 +0200

    bugfix

--- Board.py
+++ Board.py
@@ -1,9 +1,9 @@
 from Tile import Tile
 from River import River
-from numpy import array
+import numpy as np
 class Board:
     rivers = list[River]
-    tiles = array([],dtype=Tile)
+    tiles = np.array([],dtype=Tile)
 
     def tile_coords(tile:Tile):
         pass
